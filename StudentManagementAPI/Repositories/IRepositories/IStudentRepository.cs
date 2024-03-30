92057da7899c19b2b5b4e3a5afca0d5f638981c4
20127670 <nonameno1f@gmail.com>
2024-03-30 20:08:18 +0700

    feat(core): adding StudentRepository Pattern

---
+++ StudentManagementAPI/Repositories/IRepositories/IStudentRepository.cs
@@ -0,0 +1,23 @@
+﻿using StudentManagementAPI.Models;
+
+namespace StudentManagementAPI.Repositories.IRepositories
+{
+    public interface IStudentRepository
+    {
+        ICollection<Student> GetStudents();
+
+        Student GetStudent(int studentId);
+
+        bool ExistsStudentById(int studentId);
+        bool ExistsStudentByStudentId(string studentStringId);
+        bool ExistsStudentByName(string studentName);
+
+        bool CreateStudent(Student student);
+
+        bool UpdateStudent(Student student);
+
+        bool DeleteStudent(Student student);
+
+        bool Save();
+    }
+}
---
+++ StudentManagementAPI/Repositories/StudentRepository.cs
@@ -0,0 +1,60 @@
+﻿using StudentManagementAPI.Models;
+using StudentManagementAPI.Repositories.IRepositories;
+using StudentManagementAPI.Datas;
+
+namespace StudentManagementAPI.Repositories
+{
+    public class StudentRepository : IStudentRepository
+    {
+        private readonly StudentDatabaseContext _studentDbContext;
+        public StudentRepository(StudentDatabaseContext studentDbContext)
+        {
+            _studentDbContext = studentDbContext;
+        }
+        public ICollection<Student> GetStudents()
+        {
+            return _studentDbContext.Students.OrderBy(x => x.StudentId).ToList();
+        }
+
+        public Student GetStudent(int studentId)
+        {
+            return _studentDbContext.Students.FirstOrDefault(x => x.Id == studentId);
+        }
+
+        public bool ExistsStudentById(int studentId)
+        {
+            return _studentDbContext.Students.Any(x => x.Id == studentId);
+        }
+        public bool ExistsStudentByStudentId(string studentStringId)
+        {
+            return _studentDbContext.Students.Any(x => x.StudentId.ToLower() == studentStringId.ToLower());
+        }
+        public bool ExistsStudentByName(string studentName)
+        {
+            return _studentDbContext.Students.Any(x => x.FirstName.ToLower().Trim() == studentName.ToLower().Trim());
+        }
+
+        public bool CreateStudent(Student student)
+        {
+            _studentDbContext.Students.Add(student);
+            return Save();
+        }
+
+        public bool DeleteStudent(Student student)
+        {
+            _studentDbContext.Students.Remove(student);
+            return Save();
+        }
+
+        public bool UpdateStudent(Student student)
+        {
+            _studentDbContext.Students.Update(student);
+            return Save();
+        }
+
+        public bool Save()
+        {
+            return _studentDbContext.SaveChanges() >= 0 ? true : false;
+        }
+    }
+}
