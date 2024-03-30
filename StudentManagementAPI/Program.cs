b5f60a471455e88b35d1e2139009052c1816c1c9
20127670 <nonameno1f@gmail.com>
2024-03-30 20:14:06 +0700

    chore: adding some default files

---
+++ StudentManagementAPI/Program.cs
@@ -0,0 +1,54 @@
+using StudentManagementAPI.Repositories;
+using StudentManagementAPI.Repositories.IRepositories;
+using StudentManagementAPI.Datas;
+using StudentManagementAPI.Models.StudentMSMapper;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// 1. Setup DbContext
+builder.Services.AddDbContext<StudentDatabaseContext>(options =>
+{
+    string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseSqlServer(connectionString);
+});
+
+// 2. Add Repository Patterns
+builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+
+
+// 4. AutoMapper
+builder.Services.AddAutoMapper(typeof(StudentMSMappings));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+// var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+// 3. Use Swagger
+// app.UseSwagger();
+// app.UseSwaggerUI(options =>
+//     {
+//         foreach (var desc in provider.ApiVersionDescriptions)
+//         {
+//             options.SwaggerEndpoint($"/swagger/{desc.GroupName}/swagger.json",
+//                 desc.GroupName.ToUpperInvariant());
+//         }
+//         options.RoutePrefix = "";
+//     }
+// );
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
